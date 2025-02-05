ffd8c1504d4f46d7324fe44ea3b10ffe5b71af51
Choollol <67297627+Choollol@users.noreply.github.com>
2025-02-04 17:56:41 -0800

    Create automated tests for addToCart()

---
+++ tests-jasmine/data/cartTest.js
@@ -0,0 +1,71 @@
+import * as cartModule from "../../data/cart.js";
+
+describe("test suite: addToCart", () => {
+  it("adds an existing product to the cart", () => {
+    spyOn(localStorage, "setItem");
+
+    spyOn(localStorage, "getItem").and.callFake(() => {
+      return JSON.stringify([{
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 1,
+        deliveryOptionId: "1"
+      }]);
+    });
+    cartModule.loadFromStorage();
+
+    cartModule.addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6", 1);
+    expect(cartModule.cart.length).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(cartModule.cart[0].productId).toEqual("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
+    expect(cartModule.cart[0].quantity).toEqual(2);
+  });
+
+  it("adds a new product to the cart", () => {
+    spyOn(localStorage, "setItem");
+
+    spyOn(localStorage, "getItem").and.callFake(() => {
+      return JSON.stringify([]);
+    });
+    cartModule.loadFromStorage();
+    
+    cartModule.addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6", 1);
+    expect(cartModule.cart.length).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(cartModule.cart[0].productId).toEqual("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
+    expect(cartModule.cart[0].quantity).toEqual(1);
+  });
+
+  it("adds two new products to the cart", () => {
+    spyOn(localStorage, "setItem");
+
+    spyOn(localStorage, "getItem").and.callFake(() => {
+      return JSON.stringify([]);
+    });
+    cartModule.loadFromStorage();
+    
+    cartModule.addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6", 2);
+    expect(cartModule.cart.length).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(cartModule.cart[0].productId).toEqual("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
+    expect(cartModule.cart[0].quantity).toEqual(2);
+  });
+
+  it("adds two of an existing product to the cart", () => {
+    spyOn(localStorage, "setItem");
+
+    spyOn(localStorage, "getItem").and.callFake(() => {
+      return JSON.stringify([{
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 1,
+        deliveryOptionId: "1"
+      }]);
+    });
+    cartModule.loadFromStorage();
+    
+    cartModule.addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6", 2);
+    expect(cartModule.cart.length).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(cartModule.cart[0].productId).toEqual("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
+    expect(cartModule.cart[0].quantity).toEqual(3);
+  });
+});
---
+++ tests-jasmine/utils/moneyTest.js
@@ -0,0 +1,15 @@
+import * as moneyModule from "../../scripts/utils/money.js";
+
+describe("test suite: formatCurrency", () => {
+  it("converts cents into dollars", () => {
+    expect(moneyModule.formatCurrency(2095)).toEqual("20.95");
+  });
+
+  it("works with 0", () => {
+    expect(moneyModule.formatCurrency(0)).toEqual("0.00");
+  });
+
+  it("rounds up to the nearest cent", () => {
+    expect(moneyModule.formatCurrency(2000.5)).toEqual("20.01");
+  });
+});

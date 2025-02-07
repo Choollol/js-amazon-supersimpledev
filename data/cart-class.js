ac0747488c863f1f65a145d0007cede27d6b1cdd
Choollol <67297627+Choollol@users.noreply.github.com>
2025-02-06 19:03:18 -0800

    Create class version of cart

---
+++ data/cart-class.js
@@ -0,0 +1,65 @@
+class Cart {
+  cartItems;
+  localStorageKey;
+
+  constructor(localStorageKey) {
+    this.localStorageKey = localStorageKey;
+
+    this.loadFromStorage();
+  }
+
+  loadFromStorage() {
+    this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey)) ?? [{
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 1,
+      deliveryOptionId: "1",
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 2,
+      deliveryOptionId: "2",
+    }];
+  }
+
+  saveCartToStorage() {
+    localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+  }
+
+  addToCart(productId, quantity) {
+    const matchingItem = this.findCartItemById(productId);
+
+    if (matchingItem) {
+      matchingItem.quantity += quantity;
+    }
+    else {
+      this.cartItems.push({
+        productId,
+        quantity,
+        deliveryOptionId: "1",
+      });
+    }
+    this.saveCartToStorage();
+  }
+
+  removeFromCart(productId) {
+    this.cartItems = this.cartItems.filter((cartItem) => cartItem.productId !== productId);
+    this.saveCartToStorage();
+  }
+
+  updateDeliveryOption(productId, deliveryOptionId) {
+    const matchingItem = this.findCartItemById(productId);
+
+    matchingItem.deliveryOptionId = deliveryOptionId;
+    this.saveCartToStorage();
+  }
+
+  findCartItemById(productId) {
+    return this.cartItems.find((cartItem) => cartItem.productId === productId);
+  }
+}
+
+const cart = new Cart("cart-oop");
+const businessCart = new Cart("cart-business");
+
+console.log(cart);
+console.log(businessCart);
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import * as orderSummaryModule from "./checkout/orderSummary.js";
 import * as paymentSummaryModule from "./checkout/paymentSummary.js";
-import "../data/cart-oop.js";
+import "../data/cart-class.js";
 
 orderSummaryModule.renderOrderSummary();
 paymentSummaryModule.renderPaymentSummary();

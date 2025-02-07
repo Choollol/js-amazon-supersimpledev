6cb275e5cd2626924f82da21f3c7cfa9f458f3df
Choollol <67297627+Choollol@users.noreply.github.com>
2025-02-06 19:57:57 -0800

    Add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import * as orderSummaryModule from "./checkout/orderSummary.js";
 import * as paymentSummaryModule from "./checkout/paymentSummary.js";
 // import "../data/cart-class.js";
+import "../data/backend-practice.js";
 
 orderSummaryModule.renderOrderSummary();
 paymentSummaryModule.renderPaymentSummary();
